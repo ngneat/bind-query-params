0869dc70e7730ac7b3624e1b9826a9beb382df73
Shahar Kazaz <shahar.kazaz@gmail.com>
2023-05-08 17:35:53 +0300

    refactor: 💡 add assertInjectionContext

--- projects/ngneat/bind-query-params/src/lib/bind-query-params-factory.ts
+++ projects/ngneat/bind-query-params/src/lib/bind-query-params-factory.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, inject, Injector, DestroyRef } from '@angular/core';
+import { Inject, Injectable, inject, Injector, DestroyRef, assertInInjectionContext } from '@angular/core';
 import { Router } from '@angular/router';
 import { BindQueryParamsManager } from './bind-query-params-manager';
 import { BIND_QUERY_PARAMS_OPTIONS } from './options';
@@ -10,6 +10,7 @@ export class BindQueryParamsFactory {
   private options = inject(BIND_QUERY_PARAMS_OPTIONS);
 
   create<T>(defs: QueryDefOptions<T>[] | QueryDefOptions<T>, options?: CreateOptions): BindQueryParamsManager<T> {
+    !options?.injector && assertInInjectionContext(this.create);
     const injector = options?.injector ?? inject(Injector);
     const manager = new BindQueryParamsManager<T>(this.router, defs, this.options, options);
 
